105c279f7370787b5702d3d87f31b059b42bffdc
khanhct <khanhdoan01hd@gmail.com>
2026-01-09 20:39:24 +0700

    them chuc nang dang ki

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,11 @@
 import bcrypt from 'bcrypt';
 import User from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import crypto from 'crypto';
+import Session from '../models/Session.js';
 
-
+const ACCESS_TOKEN_TTL = '30m';
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000;
 
 export const signUp = async (req, res) => {
     try {
@@ -32,5 +36,53 @@ export const signUp = async (req, res) => {
     } catch (error) {
         console.error('loi khi goi signup', error);
         res.status(500).json({message: 'Loi he thong'});
+    }
+}
+
+export const signIn = async (req, res) => {
+    try {
+        // lay input
+        const {username, password} = req.body;
+        if(!username || !password){
+            return res.status(400).json({message: 'thieu username hoac password'});
+        }
+
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(401).json({message: 'username hoac password khong dung'});
+        }
+
+        // kiem tra password
+        const passwordCorrect = await bcrypt.compare(password, user.hashPassword);
+        if(!passwordCorrect){
+            return res.status(401).json({message: 'usernname hoac password khong dung'});
+        }
+        
+        // neu khop tao accessToken voi JWT
+        const accessToken = jwt.sign({userId: user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: ACCESS_TOKEN_TTL});
+
+        // tao refreshToken
+        const refreshToken = crypto.randomBytes(64).toString('hex');
+
+        // tao session moi de luu refreshToken
+        await Session.create({
+            userId: user._id,
+            refreshToken,
+            expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+        });
+        
+        // tra refreshToken ve cookie
+        res.cookie('refreshToken', refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: REFRESH_TOKEN_TTL,
+        });
+
+        // tra accsessToken ve res
+        res.status(200).json({message: `user ${user.displayName} da login, accessToken: ${accessToken}`});
+    } catch (error) {
+        console.error('loi khi goi signin', error);
+        res.status(500).json({message: 'loi he thong'});
     }
 }

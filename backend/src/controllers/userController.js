617cd672b53d24acb8c390bb7168cf9aff1adff1
khanhct <khanhdoan01hd@gmail.com>
2026-01-13 01:04:26 +0700

    update userController

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,12 @@
 export const authMe = async (req, res) => {
-    return res.status(200).json({message: 'user'});
+    try {
+        const user = req.user; // lay tu middleweare
+
+        return res.status(200).json({
+            user
+        });
+    } catch (error) {
+        console.error('loi khi goi authMe', error);
+        return res.status(500).json({ message: 'loi he thong' });
+    }
 }
